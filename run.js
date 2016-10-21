39de63f28546d4b36c088dc7c4e303b2db46a727
白植泵 <731615400@qq.com>
2016-10-21 15:47:20 +0800

    deploy

--- run.js
+++ run.js
@@ -1,31 +1,49 @@
-
 const http = require('http');
-const util = require('util');
-// const getRawBody = require('raw-body');
+const {createHmac} = require('crypto');
+const {execFile} = require('child_process');
 
-let server = http.createServer(function (request, response) {
-    console.log(request.method);
-    console.log(request.url);
-    if (request.method === 'POST' && request.url === '/upblog') {
-        let data = '';
-        request.on('data', function (chunk) {
-            data += chunk;
-        })
-        request.on('end', function () {
-            let transcode = '';
-            let dataJson = {};
-            console.log(util.inspect(data));
-            transcode = decodeURIComponent(util.inspect(data));
-            try {
-                dataJson = JSON.parse(transcode.replace(/payload=|\'/g, ''));
-            } catch (err) {
-                console.log(err);
-            }
-            console.log(dataJson);
-            response.statusCode = 200;
-            response.end(util.inspect(data));
-        })
+const {PORT, URL_PATH, SECRET, COMMEND_PATH} = require('./config/config.json');
+
+let hexo = '';
+
+let server = http.createServer( (request, response) => {
+
+    let {method, url} = request;
+    if (!(method === 'POST' && url === URL_PATH)) {
+        response.end('you get lost?');
     }
+
+    let data;
+    request.on('data', (chunk) => data += chunk);
+
+    request.on('end', () => {
+
+        let dataString = decodeURIComponent(data);
+
+        try {
+            //验证secret
+            let signature = 'sha1=' + createHmac('sha1', SECRET).update(data).digest('hex');
+            let checkSignature = signature === request.headers['x-hub-signature'];
+            if (!checkSignature) {
+                throw Error('check signature failed!');
+            }
+
+            hexo && hexo.kill('SIGINT');
+            hexo = execFile(COMMEND_PATH, [], {}, (err, stdout, stderr) => {
+                if (err) {
+                    throw Error(err);
+                }
+                console.info(stdout);
+                let dataJson = JSON.parse(dataString.replace(/payload=|\'/g, ''));
+                response.end(dataJson);
+            })
+
+        } catch (err) {
+            console.error(err);
+            response.statusCode = 400;
+            response.end('update blog failed!');
+        }
+    })
 })
 
-server.listen(2333, () => console.log('listen to', 2333));
+server.listen(PORT, () => console.log('listen to', PORT));
